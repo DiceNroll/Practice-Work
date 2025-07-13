0f2ecb7e5d1c394b66675935d087a167415a9b8f
2op <60447606+DiceNroll@users.noreply.github.com>
2025-07-13 23:27:52 +0300

    -

--- Module11/11.13-5.py
+++ Module11/11.13-5.py
@@ -1,4 +1,5 @@
 from math import pi
+
 EarthVolume = 1.08321 * 10 ** 12
 PlanetVolume = float(input('Введите радиус случайной планеты: '))**3 * 4 / 3 * pi
 if EarthVolume > PlanetVolume: print('Объем планеты Земля больше в', round(EarthVolume/PlanetVolume,3))
